d8e3e6c8bd9c38eac231a4c2ebf8379eacd96304
Oleksandr Zozulia <freemanlex@gmail.com>
2022-09-08 21:42:43 +0300

    first commit

---
+++ verification/tests.py
@@ -0,0 +1,45 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": [[], [1, 2, 3]],
+            "answer": None
+        },
+        {
+            "input": [[1], [1]],
+            "answer": 1
+        },
+        {
+            "input": [[3], [1, 2, 3]],
+            "answer": 3
+        }, 
+
+    ],
+    "Extra": [
+        {
+            "input": [[], []],
+            "answer": None
+        },
+        {
+            "input": [[3, 4, 5], [1, 2, 3, 4, 5]],
+            "answer": 3
+        },
+        {
+            "input": [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]],
+            "answer": 1
+        },
+        {
+            "input": [[1, 2, 3, 4, 5], []],
+            "answer": None
+        }
+    ]
+}
